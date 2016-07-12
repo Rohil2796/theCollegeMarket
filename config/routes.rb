08a72861c8f99306712c4262fea7f48870d0b1d7
Bryan Nix <bpnix10@gmail.com>
2016-07-12 00:20:59 -0400

    routing again

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   root   'pages#home'
   get    '/hello',    to: 'pages#hello'
   get    '/home',	 to: 'pages#home'
-  get    '/help',    to: 'pages#help'
   get    '/about',   to: 'pages#about'
   get    '/contact', to: 'pages#contact'
