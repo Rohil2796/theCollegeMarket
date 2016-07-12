f61af569fa71d8e3705404e0cb6e58454b183314
Bryan Nix <bpnix10@gmail.com>
2016-07-12 00:18:02 -0400

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,3 @@ Rails.application.routes.draw do
   get    '/help',    to: 'pages#help'
   get    '/about',   to: 'pages#about'
   get    '/contact', to: 'pages#contact'
-  get    '/signup',  to: 'users#new'
- 
-  resources :users do
-    member do
-      get :following, :followers
-    end
-  end
-  resources :account_activations, only: [:edit]
-  resources :password_resets,     only: [:new, :create, :edit, :update]
-  resources :microposts,          only: [:create, :destroy]
-  resources :relationships,       only: [:create, :destroy]
-end
