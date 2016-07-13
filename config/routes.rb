4d4f6a878d278868a4c7bdcb048770f38112e153
Bryan Nix <bpnix10@gmail.com>
2016-07-12 21:36:42 -0400

    bugs again

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root   'pages#home'
   get    '/hello',    to: 'pages#hello'
-  get    '/home',	 to: 'pages#home'
   get    '/about',   to: 'pages#about'
   get    '/contact', to: 'pages#contact'
 
