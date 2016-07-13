7d3b07a9ceed6cc7ee80e267792106e6d8603d75
Bryan Nix <bpnix10@gmail.com>
2016-07-12 21:43:37 -0400

    dk

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
   root   'pages#home'
   get    '/hello',    to: 'pages#hello'
+  get    '/home',	 to: 'pages#home'
+  get    '/help',    to: 'pages#help'
   get    '/about',   to: 'pages#about'
   get    '/contact', to: 'pages#contact'
-
+  get    '/signup',  to: 'users#new'
+ 
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
