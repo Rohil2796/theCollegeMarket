0ad3518c3e2c14b840f750765dc3c47f1e2ac02f
Bryan Nix <bpnix10@gmail.com>
2016-07-23 19:25:46 -0400

    change

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,4 @@ Rails.application.routes.draw do
   get    '/about',   to: 'pages#about'
   get    '/contact', to: 'pages#contact'
   get    '/signup',  to: 'users#new'
- 
-  resources :users do
-    member do
-      get :following, :followers
-    end
-  end
-  resources :account_activations, only: [:edit]
-  resources :password_resets,     only: [:new, :create, :edit, :update]
-  resources :microposts,          only: [:create, :destroy]
-  resources :relationships,       only: [:create, :destroy]
 end
