fbb496325e84a0a055bb897f06cb59dda2d15801
Bryan Nix <bpnix10@gmail.com>
2016-07-23 19:39:47 -0400

    update

---
+++ app/assets/javascripts/pages.js
@@ -0,0 +1,3 @@
+    $('.cd-nav-trigger').click(function() {
+        $(this).addClass(".navigation-is-open").removeClass(".navigation-is-open");
+    });
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
   root   'pages#home'
-  get    'pages/hello',    to: 'pages#hello'
-  get    'pages/home',	 to: 'pages#home'
-  get    'pages/help',    to: 'pages#help'
-  get    'pages/about',   to: 'pages#about'
-  get    'pages/contact', to: 'pages#contact'
+  get    '/hello',    to: 'pages#hello'
+  get    '/home',	 to: 'pages#home'
+  get    '/help',    to: 'pages#help'
+  get    '/about',   to: 'pages#about'
+  get    '/contact', to: 'pages#contact'
+  get    '/signup',  to: 'users#new'
+ 
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
