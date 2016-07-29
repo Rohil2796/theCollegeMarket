8017b30fd02957d5e276718a84b1cce21f8078d9
Bryan Nix <bpnix10@gmail.com>
2016-07-28 21:14:59 -0400

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root   'pages#home'
-  get    'pages/hello' 
-  get    'pages/home' 
-  get    'pages/help' 
-  get    'pages/about'
-  get    'pages/contact'
-  get  '/signup',  to: 'users#new'
+  get    '/hello' ,  to: 'pages#hello'
+  get    '/home' ,  to: 'pages#home'
+  get    '/help' ,  to: 'pages#help'
+  get    '/about',  to: 'pages#about'
+  get    '/contact',  to: 'pages#contact'
+  get    '/signup',  to: 'users#new', as: :signup_path
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users 
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
   test "invalid signup information" do
-    get signup_path
+    get '/signup'
     assert_no_difference 'User.count' do
       post users_path, params: { user: { name:  "",
                                          email: "user@invalid",
@@ -16,7 +16,7 @@ assert_select 'div#error_explanation'
   end
 
   test "valid signup information" do
-    get signup_path
+    get '/signup'
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name:  "Example User",
                                          email: "user@example.com",
