32a21f98b2fbed8345814509d81e2584ef91832c
Bryan Nix <bpnix10@gmail.com>
2016-07-29 10:08:22 -0400

    editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-	
+	before_action :logged_in_user, only: [:edit, :update]
+
 	def show 
 		@user = User.find(params[:id])
 	end 
@@ -8,7 +9,7 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
-  def create
+ def create
     @user = User.new(user_params)
     if @user.save
       log_in @user
@@ -19,10 +20,31 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
 	private
 
 		def user_params
 			params.require(:user).permit(:firstName, :lastName, :email, :password, :password_confirmation)
+		end
+
+		def logged_in_user
+			unless logged_in?
+				flash[:danger] = "Please log in."
+				redirect_to login_url
+			end
 		end
 
 end
