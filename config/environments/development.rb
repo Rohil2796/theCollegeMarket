fe1b899936693a0650ff3dfc16b9e1031def5c24
Bryan Nix <bpnix10@gmail.com>
2016-07-29 23:27:35 -0400

    dkd

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  config.action_mailer.default_url_options = { :host => 'localhost' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   
 
   # In the development environment your application's code is reloaded on
