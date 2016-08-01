47078909897e30f317e1055553b10761193dd8a2
Bryan Nix <bpnix10@gmail.com>
2016-07-31 22:23:09 -0400

    ldkfj

--- test/integration/tbposts_interface_test.rb
+++ test/integration/tbposts_interface_test.rb
@@ -12,22 +12,22 @@ class TbpostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select 'div.pagination'
     # Invalid submission
     assert_no_difference 'Tbpost.count' do
-      post tbpost_path, params: { tbpost: { content: "" } }
+      post tbposts_path, params: { tbpost: { content: "" } }
     end
     assert_select 'div#error_explanation'
     # Valid submission
     content = "This tbpost really ties the room together"
     assert_difference 'Tbpost.count', 1 do
-      post tbposts_path, params: { micropost: { content: content } }
+      post tbposts_path, params: { tbpost: { content: content } }
     end
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
     # Delete post
     assert_select 'a', text: 'delete'
-    first_tbpost = @user.tbposts.paginate(page: 1).first
-    assert_difference 'Micropost.count', -1 do
-      delete tbposts_path(first_tbpost)
+    first_tbpost = @user.tbpost.paginate(page: 1).first
+    assert_difference 'Tbpost.count', -1 do
+      delete tbpost_path(first_tbpost)
     end
     # Visit different user (no delete links)
     get user_path(users(:rohil))
