9fa752edf39ecd3ef75efe6edc6f44e69887df34
Bryan Nix <bpnix10@gmail.com>
2016-08-07 21:39:42 -0400

    dlk

--- db/migrate/20160805172111_add_status_to_posts.rb
+++ db/migrate/20160805172111_add_status_to_posts.rb
@@ -1,7 +1,8 @@
 class AddStatusToPosts < ActiveRecord::Migration[5.0]
   def change
-    add_column :posts, :status, :integer, default: 0, 
-    add_column :posts, :offerrequest, :integer, default: 0,
+    add_column :posts, :status, :integer, default: 0
+    add_column :posts, :offerrequest, :integer, default: 0
+
     add_column :posts, :posttype, :integer, default: 0
   end
 end
