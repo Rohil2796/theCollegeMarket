c39944bf5b303edbbabf8cefd01af0b5ef613d97
Bryan Nix <bpnix10@gmail.com>
2016-08-23 13:51:42 -0400

    hey

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,7 +27,7 @@ end
 
   def update
       @post = Post.find(params[:id])
-      if @Post.update_attributes(user_params)
+      if @Post.update_attributes(post_params)
         flash[:success] = "Post updated"
         redirect_to @post
       else
