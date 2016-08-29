3e9528e6f6ec9bc459c56f117f670d1fe0866f43
Bryan Nix <bpnix10@gmail.com>
2016-08-29 02:00:30 -0400

    hey

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ before_action :correct_user,   only: :destroy
 
 def show
    @post = Post.find(params[:id])
-   @attached_photos = AttachedAsset.order('post_id ASC')
+   @attached_photos = @post.AttachedAsset.order('post_id ASC')
 end
 
 def new
