e0a44d1fba0d6ff4a53a2f7799debbe4c186df07
Bryan Nix <bpnix10@gmail.com>
2016-08-30 19:35:25 -0400

    hey

--- db/migrate/20160830215336_modify_price_posts_column.rb
+++ db/migrate/20160830215336_modify_price_posts_column.rb
@@ -2,5 +2,6 @@ class ModifyPricePostsColumn < ActiveRecord::Migration[5.0]
   def change
   	change_column :posts, :price, :integer, :default => 0, :null => false
   	change_column :posts, :category_id, :integer, :default => 0, :null => false
+  	change_column :posts, :postitemservice, :integer, :default => 0
   end
 end
