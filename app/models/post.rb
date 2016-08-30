f0cf37262069f1880d7ff693a76f3207ec230c92
Bryan Nix <bpnix10@gmail.com>
2016-08-30 18:47:45 -0400

    hey

--- app/models/post.rb
+++ app/models/post.rb
@@ -21,6 +21,7 @@ class Post < ApplicationRecord
   validates :description, length: { maximum: 1000 }
   VALID_PRICE_REGEX = /\A[0-9]\z/
   validates :price, presence: true, length: { maximum: 5, message: 'Price must be a number, and less than $10,000'}, format: { with: VALID_PRICE_REGEX, message: 'Price must be a number, and less than $10,000' }
+  validates :category_id, presence: true
 
   # validates :expiredate,     date: { after: Proc.new { Time.now },
   #      dd           					before: Proc.new { Time.now + 1.year } }
--- db/migrate/20160830215336_modify_price_posts_column.rb
+++ db/migrate/20160830215336_modify_price_posts_column.rb
@@ -1,5 +1,6 @@
 class ModifyPricePostsColumn < ActiveRecord::Migration[5.0]
   def change
   	change_column :posts, :price, :integer, :default => 0, :null => false
+  	change_column :posts, :category_id, :integer, :default => 0, :null => false
   end
 end
