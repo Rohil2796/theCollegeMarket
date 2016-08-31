90ff593d96053c937f02a93920b89e2fa0cdb810
Bryan Nix <bpnix10@gmail.com>
2016-08-30 23:02:27 -0400

    hey

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,11 @@
 class PagesController < ApplicationController
-
+  
   def about
   end
 
  def home
     if logged_in?
-      @post = Post.find(params[:id])
+      @post  = current_user.post.build
       @feed_items = current_user.feed.paginate(page: params[:page])
       @attached_photos = @post.attached_assets.order('post_id ASC').take(1)
     end
