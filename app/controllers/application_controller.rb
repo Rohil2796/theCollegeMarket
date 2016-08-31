0a956eb44c40631bf2d19e1b6d445150913d8415
Bryan Nix <bpnix10@gmail.com>
2016-08-30 22:34:18 -0400

    hey

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   
-  before_filter :post
+
 
   
    rescue_from ActiveRecord::RecordNotFound do
