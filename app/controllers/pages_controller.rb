efe48bd0f3d3d6624080bdd24cabb8fdc6bea432
Bryan Nix <bpnix10@gmail.com>
2016-08-30 22:53:00 -0400

    hey

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,13 @@
 class PagesController < ApplicationController
+  before_filter :post 
   def about
   end
 
  def home
     if logged_in?
-      @post = Post.find(params[:id])
+      @post  = current_user.post.build
       @feed_items = current_user.feed.paginate(page: params[:page])
-      @attached_photos = @post.attached_assets.order('post_id ASC').take(1)
+      
     end
   end
 
@@ -15,6 +16,9 @@ class PagesController < ApplicationController
 
   def contact
   end 
-
+ def post
+   @post = Post.find(params[:id])
+   @attached_photos = @post.attached_assets.order('post_id ASC').take(1)
+  end
   
 end
